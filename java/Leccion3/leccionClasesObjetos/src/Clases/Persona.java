869d3f93c9fa48e0b2ae5f8e6cd3cb5712661bbc
Gonzalo Bucca <gonzalobucca1@gmail.com>
2024-09-18 15:38:20 -0300

    Comiteamos leccion 3 clases y objetos en Java

--- java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
+++ java/Leccion3/leccionClasesObjetos/src/Clases/Persona.java
@@ -6,4 +6,10 @@ public class Persona {
    //Atributos de la clase (Caracteristicas)
     String nombre;
     String apellido;
+    
+    // Metodos de las clases (Acciones)
+    public void obtenerInformacion(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
 }
